7153a42c074a604b7525c617ea8191f76856d0d4
jgatjens <jgatjens@gmail.com>
2023-05-09 15:48:48 -0600

    custom middleware ip blocking

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+// eslint-disable-next-line
+export default async function (req: NextRequest) {
+  const res = NextResponse.next();
+  let ip = req.ip ?? req.headers.get("x-real-ip");
+  const forwardedFor = req.headers.get("x-forwarded-for");
+  if (!ip && forwardedFor) {
+    ip = forwardedFor.split(",").at(0) ?? "Unknown";
+  }
+
+  if (ip) {
+    res.cookies.set("user-ip", ip, {
+      httpOnly: false,
+    });
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
